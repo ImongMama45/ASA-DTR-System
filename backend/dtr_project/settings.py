9518b087e3dac88527bfdc23b9f875c67a72361e
ImongMama45 <quertrussellalisan@gmail.com>
2026-04-27 15:08:43 +0800

    Update settings.py

--- backend/dtr_project/settings.py
+++ backend/dtr_project/settings.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 print(SECRET_KEY)
 DEBUG = False
 
