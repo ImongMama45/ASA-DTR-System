302f0eba6bea958a118b9ed332c1283c4eb12cf0
ImongMama45 <quertrussellalisan@gmail.com>
2026-04-27 14:59:11 +0800

    Update settings.py

--- backend/dtr_project/settings.py
+++ backend/dtr_project/settings.py
@@ -97,3 +97,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
 }
+
+CSRF_TRUSTED_ORIGINS = ["https://asa-dtr-system.onrender.com"]
