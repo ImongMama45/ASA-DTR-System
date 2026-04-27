40ca043f61e4d39e5d1ee1ec7dd9fc1d339059d3
ImongMama45 <quertrussellalisan@gmail.com>
2026-04-27 14:16:59 +0800

    Update urls.py

--- backend/dtr_project/urls.py
+++ backend/dtr_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 
+def home(request):
+    return HttpResponse("Backend is running!")
+
 urlpatterns = [
+    path('', home),  # 👈 ADD THIS
     path('admin/', admin.site.urls),
     path('api/', include('dtr_api.urls')),
 ]
