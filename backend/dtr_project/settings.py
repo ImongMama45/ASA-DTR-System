50b5f3e279e3381a6d94d7fc1afdeedce2d5db52
ImongMama45 <quertrussellalisan@gmail.com>
2026-05-15 15:21:05 +0800

    'Ayusi'

--- backend/dtr_project/settings.py
+++ backend/dtr_project/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'dtr_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
     )
 }
 
