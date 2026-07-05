6bec8e9e40ac970be829004dc8d91014f37c2b07
ImongMama45 <quertrussellalisan@gmail.com>
2026-07-06 01:49:27 +0800

    Admin Fixed

--- backend/dtr_api/admin.py
+++ backend/dtr_api/admin.py
@@ -1,26 +1,27 @@
 from django.contrib import admin
-from .models import Employee, DTRBatch, SyncLog, FundPayment, User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Employee, DTRBatch, SyncLog, FundPayment, UserProfile
 
-class FundPaymentInline(admin.StackedInline):
+
+class FundPaymentInline(admin.TabularInline):
     """Show FundPayment records directly inside the Employee admin form."""
     model = FundPayment
-    extra = 0  # Don't show extra empty rows by default
-    readonly_fields = ('amount', 'date_formatted')
+    extra = 0
+    readonly_fields = ('year', 'month', 'cutoff', 'amount', 'modified_at')
     fk_name = 'employee'
+    can_delete = False
 
-    def date_formatted(self, obj):
-        """A helper to show the date in a nicer format inside the admin."""
-        if not obj.date_created:
-            return "Never"
-        return obj.date_created.strftime('%Y-%m-%d %H:%M')
-    date_formatted.short_description = 'Date Paid'
+    def has_add_permission(self, request, obj=None):
+        return False
+
 
 @admin.register(FundPayment)
 class FundPaymentAdmin(admin.ModelAdmin):
-    list_display = ['employee_name', 'year', 'month', 'cutoff', 'amount', 'date_created']
+    list_display = ['employee_name', 'year', 'month', 'cutoff', 'amount', 'modified_at']
     list_filter = ['year', 'month', 'cutoff']
     search_fields = ['employee__name']
-    readonly_fields = ('date_created',)
+    readonly_fields = ('modified_at',)
 
     def employee_name(self, obj):
         if not obj.employee:
@@ -28,12 +29,17 @@ class FundPaymentAdmin(admin.ModelAdmin):
         return obj.employee.name
     employee_name.short_description = 'Employee'
 
+
+# Unregister the default User admin, then re-register with our customisation
+admin.site.unregister(User)
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ['username', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser']
     list_filter = ['is_staff', 'is_active', 'is_superuser']
     search_fields = ['username', 'first_name', 'last_name', 'email']
     ordering = ['username']
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
@@ -50,10 +56,12 @@ class EmployeeAdmin(admin.ModelAdmin):
     list_filter = ['duty']
     inlines = [FundPaymentInline]
 
+
 @admin.register(DTRBatch)
 class DTRBatchAdmin(admin.ModelAdmin):
     list_display = ['label', 'month', 'year', 'cutoff', 'created_at']
     list_filter = ['year', 'cutoff']
+
 
 @admin.register(SyncLog)
 class SyncLogAdmin(admin.ModelAdmin):
